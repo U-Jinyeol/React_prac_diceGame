61d0f81a4fd0dce149f684e323f30103643d80e3
U-Jinyeol <100745412+U-Jinyeol@users.noreply.github.com>
2022-03-24 00:01:06 +0900

    Dice game(1차완성본)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import Board from "./Board";
+import { useState } from "react";
+import Button from "./Button";
+import "./App.css";
+import logo from "./assets/logo.png";
+
+function Random(n) {
+  return Math.ceil(Math.random() * n);
+}
 
 function App() {
+  const [myHistory, setMyHistory] = useState([]);
+  const [otherHistory, setOtherHistory] = useState([]);
+
+  const handleRollClick = () => {
+    const nextMyNum = Random(6);
+    const nextOtherNum = Random(6);
+    setMyHistory([...myHistory, nextMyNum]);
+    setOtherHistory([...otherHistory, nextOtherNum]);
+  };
+
+  const handleClearClick = () => {
+    setMyHistory([]);
+    setOtherHistory([]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <img className="App-logo" src={logo} alt="주사위게임 로고" />
+        <h1 className="App-title">주사위게임</h1>
+        <div>
+          <Button className="App-button" color="blue" onClick={handleRollClick}>
+            던지기
+          </Button>
+          <Button className="App-button" color="red" onClick={handleClearClick}>
+            처음부터
+          </Button>
+        </div>
+        <div className="App-boards">
+          <Board
+            name="나"
+            color="blue"
+            gameHistory={myHistory}
+          />
+          <Board
+            name="상대"
+            color="red"
+            gameHistory={otherHistory}
+          />
+        </div>
+      </div>
     </div>
   );
 }
